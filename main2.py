a6569306b7c747d51fe138236c1ba4ec02257647
alirezaunix <alirezaunix@gmail.com>
2023-08-30 12:54:34 +0330

    Update main.py

--- main2.py
+++ main2.py
@@ -1,106 +1,49 @@
-import streamlit as st
-import matplotlib.pyplot as plt
-from keras.models import Model
-from keras.layers import Input, Add, PReLU, Conv2DTranspose, Concatenate, MaxPooling2D, UpSampling2D, Dropout, \
-    Activation, Subtract
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import BatchNormalization
 import os
-import pydicom
-import cv2
-import numpy as np
-from tensorflow.keras.layers import Conv2D, Activation, BatchNormalization, Subtract
-from tensorflow.keras.layers import Input
-import glob
-from PIL import Image
-# Function to get DNCNN model
-def get_dncnn_model(input_channel_num):
-    inpt = Input(shape=(None, None, input_channel_num))
-    # 1st layer, Conv+relu
-    x = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same')(inpt)
-    x = Activation('relu')(x)
-    # 15 layers, Conv+BN+relu
-    for i in range(15):
-        x = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same')(x)
-        x = BatchNormalization(axis=-1, epsilon=1e-3)(x)
-        x = Activation('relu')(x)
-    # last layer, Conv
-    x = Conv2D(filters=1, kernel_size=(3, 3), strides=(1, 1), padding='same')(x)
-    x = Subtract()([inpt, x])  # input - noise
-    model = Model(inputs=inpt, outputs=x)
-    return model
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+import SimpleITK as sitk
 
-# Function to get the desired model
-def get_model(input_channel_num, model_name="srresnet"):
-    if model_name == "srresnet":
-        return get_srresnet_model(input_channel_num=input_channel_num)
-    elif model_name == "unet":
-        return get_unet_model(input_channel_num=input_channel_num, out_ch=input_channel_num)
-    elif model_name == "dncnn":
-        return get_dncnn_model(input_channel_num=input_channel_num)
-    else:
-        raise ValueError("model_name should be 'srresnet'or 'unet'")
 
-# Function to load and preprocess DICOM image
-def load_dicom_image(file):
-    ds = pydicom.dcmread(file)
-    img = (ds.pixel_array)[160:890, 160:890]
-    img = np.clip(img / factor, 0, 255)
-    img = np.uint8(img)
-    return img
+def dir_selector(folder_path='.'):
+    dirnames = [d for d in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, d))]
+    selected_folder = st.sidebar.selectbox('Select a folder', dirnames)
+    if selected_folder is None:
+        return None
+    return os.path.join(folder_path, selected_folder)
 
-def save_image(img, filename):
-    cv2.imwrite(filename, img)
 
-factor = 7
+def plot_slice(vol, slice_ix):
+    fig, ax = plt.subplots()
+    plt.axis('off')
+    selected_slice = vol[slice_ix, :, :]
+    ax.imshow(selected_slice, origin='lower', cmap='gray')
+    return fig
 
-# Streamlit web app UI
-def main():
-    # Load the logo image
-    logo_image = Image.open('download.jpg')
+st.sidebar.title('DieSitCom')
+dirname = dir_selector()
 
-    # Display the logo image
-    st.image(logo_image, caption='QMISG')
-    # st.title("Image Denoising App")
-    st.markdown("<h1 style='text-align: center; color: #00b8e6;'>Image Denoising App</h1>", unsafe_allow_html=True)
-    st.markdown(
-        "<h2 style='text-align: center; color: #565656;'>Choose a DICOM image for denoising</h2>",
-        unsafe_allow_html=True)
-
-    # Select DICOM image using file uploader
-    uploaded_file = st.file_uploader("Upload DICOM Image")
-
-    if uploaded_file is not None:
-        # Load and preprocess uploaded image
-        img = load_dicom_image(uploaded_file)
-
-        # Load and preprocess model
-        weight_file = "center_weights.011-50.997-29.16076_dncnn_CD_fantom.hdf5"
-        input_channel_num = 1
-        model = get_model(input_channel_num=input_channel_num, model_name="dncnn")
-        model.load_weights(weight_file)
-
-        # Denoise image
-        pred = model.predict(np.expand_dims(np.expand_dims(img, 2), 0))
-        denoised_image = np.clip(pred[0][:, :, 0], 0, 255).astype(dtype=np.uint8)
-
-        # Display original and denoised images
-        col1, col2 = st.columns(2)
-        with col1:
-            st.subheader("Original Image")
-            st.image(img, use_column_width=True)
-        with col2:
-            st.subheader("Denoised Image")
-            st.image(denoised_image, use_column_width=True)
-
-        # Save image button
-        if st.button("Save Denoised Image"):
-            address = st.text_input("Enter the address to save the image:")
-            save_image(denoised_image, address + "/denoised_image.png")
-            st.success("Denoised image saved successfully!")
-
-if __name__ == "__main__":
-    main()
-
-# Add footer
-st.write('Developed by QMISG')
+if dirname is not None:
+    try:
+        reader = sitk.ImageSeriesReader()
+        dicom_names = reader.GetGDCMSeriesFileNames(dirname)
+        reader.SetFileNames(dicom_names)
+        reader.LoadPrivateTagsOn()
+        reader.MetaDataDictionaryArrayUpdateOn()
+        data = reader.Execute()
+        img = sitk.GetArrayViewFromImage(data)
+    
+        n_slices = img.shape[0]
+        slice_ix = st.sidebar.slider('Slice', 0, n_slices, int(n_slices/2))
+        output = st.sidebar.radio('Output', ['Image', 'Metadata'], index=0)
+        if output == 'Image':
+            fig = plot_slice(img, slice_ix)
+            plot = st.pyplot(fig)
+        else:
+            metadata = dict()
+            for k in reader.GetMetaDataKeys(slice_ix):
+                metadata[k] = reader.GetMetaData(slice_ix, k)
+            df = pd.DataFrame.from_dict(metadata, orient='index', columns=['Value'])
+            st.dataframe(df)
+    except RuntimeError:
+        st.text('This does not look like a DICOM folder!')
